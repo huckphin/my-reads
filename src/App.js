75a4a009419efea9c08f4578e6dadb68333feb2f
Chad Rhyner <chadrhyner@gmail.com>
2018-03-25 12:49:36 -0400

    add a componentDidMount lifecycle hook to load books and store in state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
-// import * as BooksAPI from './BooksAPI'
-import './App.css'
+import React from 'react';
+import * as BooksAPI from './BooksAPI';
+import Book from './Book';
+import './App.css';
 
 class BooksApp extends React.Component {
   state = {
@@ -10,7 +11,14 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
