a1e5efbb679b4a0f6108a426984234f6e92fe032
Chad Rhyner <chadrhyner@gmail.com>
2018-04-12 00:12:13 -0400

    more work on BookSearch
    - use a <Link> component to go back to home page
    - remove the comment about search terms
    - update the snapshots for tests
    - create a new function, searchBooks

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
 
 class BookSearch extends Component {
+
+    state = {
+        books: [],
+        query: ''
+    }
+
+    searchBooks = (e) => {
+        const query = e.target.value.trim();
+        this.setState({ query: query });
+
+        if (query) {
+            BooksAPI.search(query).then((books) => {
+                if (books.length > 0) {
+                    this.setState({ books: books });
+                }
+            })
+        }
+    }
+
     render () {
+        const { books, query } = this.state;
+
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                    <Link className="close-search" to="/">Close Search</Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                        You can find these search terms here:
-                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                        you don't find a specific author or title. Every search is limited by search terms.
-                        */}
-                        <input type="text" placeholder="Search by title or author"/>
+                        <input type="text" placeholder="Search by title or author" onChange={this.searchBooks} value={query} />
                     </div>
                 </div>
                 <div className="search-books-results">
--- src/__tests__/BookSearch.js
+++ src/__tests__/BookSearch.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import BookSearch from '../BookSearch';
 import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
 
 test('will render BookSearch', () => {
     const component = renderer.create(
+        <BrowserRouter>
         <BookSearch />
+        </BrowserRouter>
     );
 
     let tree = component.toJSON();
