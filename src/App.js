c9d2d1057958de6ce5bb8d41d125d615140df5dc
Chad Rhyner <chadrhyner@gmail.com>
2018-04-12 01:48:29 -0400

    remove showSearchPage since it is not used in the implementation

--- src/App.js
+++ src/App.js
@@ -7,13 +7,6 @@ import BookSearch from './BookSearch';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     books: []
   }
 
@@ -35,6 +28,8 @@ class BooksApp extends React.Component {
   }
 
   render() {
+    const { books } = this.state;
+
     return (
       <div className="app">
         <Route exact path="/" render={() => (
@@ -43,15 +38,16 @@ class BooksApp extends React.Component {
               <h1>MyReads</h1>
             </div>
             <ListBooks
-              books={this.state.books}
-              onUpdateBook={this.onUpdateBook} />
+              books={ books }
+              onUpdateBook={ this.onUpdateBook } />
             <div className="open-search">
               <Link to="/search">Search</Link>
             </div>
           </div>
         )} />
         <Route path="/search" render={({ history }) => (
-          <BookSearch />
+          <BookSearch
+            onUpdateBook={ this.onUpdateBook } />
         )} />
       </div>
     )
